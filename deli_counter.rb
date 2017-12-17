dcd17c4d19962e866673ece39b1a1d2d62af5034
Katy Carr <khcarr9@gmail.com>
2017-12-17 14:39:04 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,10 +6,10 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    string = "The line is currently: "
+    string = "The line is currently:"
     katz_deli.each_with_index do |name,index|
       num = index+1
-      string += "#{num}. #{name}"
+      string += " #{num}. #{name}"
     end
     puts string
   end
