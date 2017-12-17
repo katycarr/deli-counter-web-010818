107f6b298360dbcf505859101f60369b0f62030b
Katy Carr <khcarr9@gmail.com>
2017-12-17 14:29:35 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 
 katz_deli = []
 
-def line do
+def line
   if katz_deli.length == 0 do
     puts "The line is current empty."
   else
@@ -10,12 +10,12 @@ def line do
   end
 end
 
-def take_a_number(katz_deli,name) do
+def take_a_number(katz_deli,name)
   num = katz_deli.length + 1
   puts "Welcome, #{name}. You are number #{num} in line."
 end
 
-def now_serving(katz_deli) do
+def now_serving(katz_deli)
   next = katz_deli[0]
   if katz_deli.length == 0 do
     puts "There is nobody waiting to be served!"
